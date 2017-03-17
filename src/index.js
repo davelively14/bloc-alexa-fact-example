1f4eda023dca41e73729b5d5157b9525cf975031
Dave Lively <davelively14@gmail.com>
2017-03-16 20:29:07 -0400

    Now twenty questions for "en"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 "use strict";
 var Alexa = require("alexa-sdk");
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+var APP_ID = "amzn1.ask.skill.108a9678-6d6c-436b-bde7-4595a973a7b6";
 
 var languageStrings = {
     "en": {
         "translation": {
             "FACTS": [
                 "The M1A1 Abrams main battle tank weighs 68 tons and can travel at speeds in excess of fifty-five miles per hour.",
-                "The Abrams has a one hundred and twenty milimeter smoothbore cannon.",
+                "The Abrams has a one hundred and twenty millimeter smoothbore cannon.",
                 "The M1A1 Abrams powertrain consists of a Honeywel multi-fuel gas turbine capable of fifteen-hundred shaft horsepower.",
-                "The Bradley Fighting Vehicle is equipped with the m two four two, twenty-five milimeter chain gun as its main weapon.",
+                "The Bradley Fighting Vehicle is equipped with the m two four two, twenty-five millimeter chain gun as its main weapon.",
                 "The m two Bradley Infantry Fighting Vehicle can accomidate up to six infrantrymen in addition to the crew.",
                 "The m three Bradley Cavalry Fighting Vehicle was designed to accomidate two scouts and additional communications equipment, surveillance gear, and ammunition.",
                 "The m four Sherman medium tank, was the most numerous battle tank used by the United States in world war two.",
@@ -18,7 +18,14 @@ var languageStrings = {
                 "The m one one three is a fully tracked armored personnel carrier was developed by Food Machinery Corporation and first fielded by Army mechanized infantry units in Vietnam in April nineteen sixty-two.",
                 "The m one one three introduced new aluminum armor that made the vehicle much lighter than earlier vehicles. It was thick enough to protect the crew and passengers against small arms fire, but light enough that the vehicle was air transportable and moderately amphibious.",
                 "The M one nine one seven was America\'s first mass-produced tank, entering production shortly before the end of World War one and a near copy of the French Renault FT. However, u s manufacturers failed to produce any in time to participate in world war i.",
-                "The Interim Armored Vehicle Striker is a family of eight-wheeled, armored fighting vehicles."
+                "The Interim Armored Vehicle Striker is a family of eight-wheeled, armored fighting vehicles.",
+                "The Stryker one hundred and five millimeter Mobile Gun System entered full production in two thousand seven.",
+                "The first use of armored tanks on the battlefield was during the Battle of the Somme on fifteen September nineteen sixteen, with mixed, but still impressive results.",
+                "The British Mark one was the world's first combat tank, entering service in August nineteen sixteen, and first used in action on the morning of fifteen September nineteen sixteen.",
+                "Beginning about nineteen forty-two, most British tank units in world war two were equipped with vehicles supplied from the United States, such as the Stuart light tank or its replacement the Sherman.",
+                "The Soviet B T tanks were a series of cavalry tanks produced in large numbers between nineteen thirty-two and nineteen forty-one. They were lightly armored, but reasonably well-armed for their time, and had the best mobility of all contemporary tanks of the world.",
+                "The T thrity-four was a medium tank produced from nineteen forty to nineteen fifty-eight. Although its armor and armament were surpassed by later tanks of the era, it has been often credited as the most effective, efficient and influential design of World War two.",
+                "The T seventy-two was the most common tank used by the Soviet Army from the nineteen seventies through the collapse of the Soviet Union."
             ],
             "SKILL_NAME" : "Armor Facts",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
@@ -31,9 +38,9 @@ var languageStrings = {
         "translation": {
             "FACTS": [
                 "The M1A1 Abrams main battle tank weighs 68 tons and can travel at speeds in excess of fifty-five miles per hour.",
-                "The Abrams has a one hundred and twenty milimeter smoothbore cannon.",
+                "The Abrams has a one hundred and twenty millimeter smoothbore cannon.",
                 "The M1A1 Abrams powertrain consists of a Honeywel multi-fuel gas turbine capable of fifteen-hundred shaft horsepower.",
-                "The Bradley Fighting Vehicle is equipped with the m two four two, twenty-five milimeter chain gun as its main weapon.",
+                "The Bradley Fighting Vehicle is equipped with the m two four two, twenty-five millimeter chain gun as its main weapon.",
                 "The m two Bradley Infantry Fighting Vehicle can accomidate up to six infrantrymen in addition to the crew.",
                 "The m three Bradley Cavalry Fighting Vehicle was designed to accomidate two scouts and additional communications equipment, surveillance gear, and ammunition.",
                 "The m four Sherman medium tank, was the most numerous battle tank used by the United States in world war two.",
@@ -42,7 +49,14 @@ var languageStrings = {
                 "The m one one three is a fully tracked armored personnel carrier was developed by Food Machinery Corporation and first fielded by Army mechanized infantry units in Vietnam in April nineteen sixty-two.",
                 "The m one one three introduced new aluminum armor that made the vehicle much lighter than earlier vehicles. It was thick enough to protect the crew and passengers against small arms fire, but light enough that the vehicle was air transportable and moderately amphibious.",
                 "The M one nine one seven was America\'s first mass-produced tank, entering production shortly before the end of World War one and a near copy of the French Renault FT. However, u s manufacturers failed to produce any in time to participate in world war i.",
-                "The Interim Armored Vehicle Striker is a family of eight-wheeled, armored fighting vehicles."
+                "The Interim Armored Vehicle Striker is a family of eight-wheeled, armored fighting vehicles.",
+                "The Stryker one hundred and five millimeter Mobile Gun System entered full production in two thousand seven.",
+                "The first use of armored tanks on the battlefield was during the Battle of the Somme on fifteen September nineteen sixteen, with mixed, but still impressive results.",
+                "The British Mark one was the world's first combat tank, entering service in August nineteen sixteen, and first used in action on the morning of fifteen September nineteen sixteen.",
+                "Beginning about nineteen forty-two, most British tank units in world war two were equipped with vehicles supplied from the United States, such as the Stuart light tank or its replacement the Sherman.",
+                "The Soviet B T tanks were a series of cavalry tanks produced in large numbers between nineteen thirty-two and nineteen forty-one. They were lightly armored, but reasonably well-armed for their time, and had the best mobility of all contemporary tanks of the world.",
+                "The T thrity-four was a medium tank produced from nineteen forty to nineteen fifty-eight. Although its armor and armament were surpassed by later tanks of the era, it has been often credited as the most effective, efficient and influential design of World War two.",
+                "The T seventy-two was the most common tank used by the Soviet Army from the nineteen seventies through the collapse of the Soviet Union."
             ],
             "SKILL_NAME" : "American Armor Facts"
         }
@@ -51,9 +65,9 @@ var languageStrings = {
         "translation": {
             "FACTS": [
                 "The M1A1 Abrams main battle tank weighs 68 tons and can travel at speeds in excess of fifty-five miles per hour.",
-                "The Abrams has a one hundred and twenty milimeter smoothbore cannon.",
+                "The Abrams has a one hundred and twenty millimeter smoothbore cannon.",
                 "The M1A1 Abrams powertrain consists of a Honeywel multi-fuel gas turbine capable of fifteen-hundred shaft horsepower.",
-                "The Bradley Fighting Vehicle is equipped with the m two four two, twenty-five milimeter chain gun as its main weapon.",
+                "The Bradley Fighting Vehicle is equipped with the m two four two, twenty-five millimeter chain gun as its main weapon.",
                 "The m two Bradley Infantry Fighting Vehicle can accomidate up to six infrantrymen in addition to the crew.",
                 "The m three Bradley Cavalry Fighting Vehicle was designed to accomidate two scouts and additional communications equipment, surveillance gear, and ammunition.",
                 "The m four Sherman medium tank, was the most numerous battle tank used by the United States in world war two.",
@@ -62,7 +76,14 @@ var languageStrings = {
                 "The m one one three is a fully tracked armored personnel carrier was developed by Food Machinery Corporation and first fielded by Army mechanized infantry units in Vietnam in April nineteen sixty-two.",
                 "The m one one three introduced new aluminum armor that made the vehicle much lighter than earlier vehicles. It was thick enough to protect the crew and passengers against small arms fire, but light enough that the vehicle was air transportable and moderately amphibious.",
                 "The M one nine one seven was America\'s first mass-produced tank, entering production shortly before the end of World War one and a near copy of the French Renault FT. However, u s manufacturers failed to produce any in time to participate in world war i.",
-                "The Interim Armored Vehicle Striker is a family of eight-wheeled, armored fighting vehicles."
+                "The Interim Armored Vehicle Striker is a family of eight-wheeled, armored fighting vehicles.",
+                "The Stryker one hundred and five millimeter Mobile Gun System entered full production in two thousand seven.",
+                "The first use of armored tanks on the battlefield was during the Battle of the Somme on fifteen September nineteen sixteen, with mixed, but still impressive results.",
+                "The British Mark one was the world's first combat tank, entering service in August nineteen sixteen, and first used in action on the morning of fifteen September nineteen sixteen.",
+                "Beginning about nineteen forty-two, most British tank units in world war two were equipped with vehicles supplied from the United States, such as the Stuart light tank or its replacement the Sherman.",
+                "The Soviet B T tanks were a series of cavalry tanks produced in large numbers between nineteen thirty-two and nineteen forty-one. They were lightly armored, but reasonably well-armed for their time, and had the best mobility of all contemporary tanks of the world.",
+                "The T thrity-four was a medium tank produced from nineteen forty to nineteen fifty-eight. Although its armor and armament were surpassed by later tanks of the era, it has been often credited as the most effective, efficient and influential design of World War two.",
+                "The T seventy-two was the most common tank used by the Soviet Army from the nineteen seventies through the collapse of the Soviet Union."
             ],
             "SKILL_NAME" : "British Armour Facts"
         }
